412d86b644e73d7cb2adbe1b3ee89e88b309dd3e
alitaalita7 <alita_24ss@mail.ru>
2024-11-14 17:37:02 +0400

    Header

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
-import Header from './components/Header/Header';
+import HeaderGround from './components/Header/HeaderGround';
+import HeaderNavigation from './components/Header/HeaderNavigation';
 
 function App() {
   return (
     <>
-      <Header/>
+      <HeaderGround/>
+      <HeaderNavigation/>
     </>
   );
 }
